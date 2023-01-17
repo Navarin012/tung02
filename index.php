c681170483213640f1d6d8d7894007d405e905bb
tncy02 <62209010024@chontech.ac.th>
2023-01-17 23:10:18 +0700

    test1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include "header.php";
+    include "banner.php";
+    include "navbar.php";
+    include "conn.php";
+?>
+
+<div class="container mt-5">
+ 
+<center>
+  <h1>gtn238 - group</h1>
+  <h1 class="nav-sty">gtn238 - group</h1>
+</center>
+
+</div>
+
+
+</body>
+
+<?php
+    include "footer.php";
+?>
+
+</html>
